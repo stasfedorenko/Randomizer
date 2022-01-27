fd4ae638616c180c24ac0ed254392b95344a499c
stasfedorenko <stasfedorenko2015@mail.ru>
2022-01-27 17:06:29 +0300

    Changed Excel Parcer

--- src/main/java/stas/controller/ExcelToString.java
+++ src/main/java/stas/controller/ExcelToString.java
@@ -6,55 +6,31 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.Iterator;
 
 public class ExcelToString {
 
     public static String parse(String nameFile) {
-
+        HashMap<Integer, String[]> groups = new HashMap<>();
         String result = "";
         try {
             FileInputStream file = new FileInputStream(nameFile);
-
-            //Create Workbook instance holding reference to .xlsx file
             XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-            //Get first/desired sheet from the workbook
             XSSFSheet sheet = workbook.getSheetAt(0);
 
-            //Iterate through each rows one by one
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-                //For each row, iterate through all the columns
+            for (Row row : sheet) {
                 Iterator<Cell> cellIterator = row.cellIterator();
-                boolean findNumGroup = false;
                 while (cellIterator.hasNext()) {
 
                     Cell cell = cellIterator.next();
-                    //Check the cell type and format accordingly
-                    switch (cell.getCellType()) {
-                        case Cell.CELL_TYPE_FORMULA:
-                            if(!findNumGroup){
-                                findNumGroup = true;
-                                result += ((int) cell.getNumericCellValue() + " ");
-                            }else{
-                                findNumGroup = false;
-                                result += ((int) cell.getNumericCellValue() + "\t");
-                            }
-                            break;
-                        case Cell.CELL_TYPE_NUMERIC:
-                            if(!findNumGroup){
-                                findNumGroup = true;
-                                result += ((int) cell.getNumericCellValue() + " ");
-                            }else{
-                                result += ((int) cell.getNumericCellValue() + "\t");
-                            }
-                            break;
-                        case Cell.CELL_TYPE_STRING:
-                            result += (cell.getStringCellValue() + " ");
-                            break;
-                    }
+                    Cell cellId = row.getCell(0);
+                    Cell cellGroup = row.getCell(2);
+                    Cell cellName = row.getCell(1);
+                    String cv = String.valueOf(cellGroup.getNumericCellValue());
+                    String[] array = new String[]{cv, cellName.getStringCellValue()};
+                    groups.put((int) cellId.getNumericCellValue(), array);
+
                 }
             }
             file.close();

7cca9e79bd325c22d0614ff4ab299b7db0edf1c0
stas <stasfedoreko571@gmail.com>
2022-01-29 15:16:20 +0300

    deleted anonim

--- anonim/random/src/main/java/com/random/DAO/StudentDAO.java
+++
@@ -1,7 +0,0 @@
-package com.random.DAO;
-
-import com.random.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentDAO extends JpaRepository<Student, Integer> {
-}
